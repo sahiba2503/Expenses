4142e6aaf8211d9f1d8b0e979478c1eab6ec56c5
sahiba <25sahiba03@gmail.com>
2026-02-10 20:31:02 +0530

    use USE-CONTEXT

--- expenses/src/App.jsx
+++ expenses/src/App.jsx
@@ -1,14 +1,35 @@
-import ExpenseTracker from "./ExpenseTracker";
-
-
+// import ExpenseTracker from "./ExpenseTracker";
+import {createContext, useContext} from "react"
+const UserContext = createContext();
 
 function App(){
+  const user = "sahiba";
+  
   return(
 <div>
-  <h2>This is my new project about expenses</h2>
-<ExpenseTracker />
-
+  <h1>This is main component 💇‍♂️💇💇‍♀️</h1>
+  <UserContext.Provider value={user}>
+    <Parent />
+    </UserContext.Provider>
 </div>
   )   
+}
+function Parent(){
+  return <Child />
+}
+function Child(){
+  const user = useContext(UserContext)
+  return(
+    <div>
+       <h2 style={{color:"blue"}}>This is chaild component 🧒🧒🧒</h2>
+      <h3 style={{color:"blue"}}>Name:{user} </h3>
+      <GrandChild />
+       </div>
+  )
+}
+function GrandChild(){
+  const user = useContext(UserContext)
+
+  return <h2>This is grand child 👶👶👶👶 , My name is {user}</h2>
 }
 export default App;
